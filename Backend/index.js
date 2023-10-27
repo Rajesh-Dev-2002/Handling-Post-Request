494b8b9723c40497005e44794e95bb3b62f07c3f
Rajesh kumar Mohanta <rajeshkumarmohanta2001@gmail.com>
2023-10-27 12:00:46 +0530

    post request handling

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,19 @@
-const express = require("express")
+const express = require("express");
 
 const app = express();
 
-app.get("/register",(req,res)=>{
-    let {user, pass} = req.query;
-    res.send(`This is GET Request,Welcome ${user}`)
-})
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.get("/register", (req, res) => {
+  let { user, pass } = req.query;
+  res.send(`This is GET Request,Welcome ${user}`);
+});
 
-app.post("/register",(req,res)=>{
-    res.send("This is POST Request")
-})
+app.post("/register", (req, res) => {
+  let { user, pas } = req.body;
+  res.send(`This is POST Request , welcome ${user}`);
+});
 let port = 5050;
-app.listen(port,()=>{
-    console.log(`Server is running at Port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running at Port ${port}`);
+});
